0feb8612c9ad2410cc5c2f5aa25d7a825e9c3828
San <18814184179@163.com>
2019-04-22 09:38:39 +0800

    344. 反转字符串

---
+++ code300/main/LeetCode344.go
@@ -0,0 +1,13 @@
+package main
+
+func reverseString(s []byte) {
+
+	for i := 0; i < len(s)/2; i++ {
+		s[i], s[len(s)-i-1] = s[len(s)-i-1], s[i]
+	}
+}
+
+func main() {
+	s := []byte{'h', 'e', 'l', 'l', '0'}
+	reverseString(s)
+}
